a51db372a7a0f0e701d2f54b31e1c3af7ddd44cd
Romaric Drigon <r.drigon@edsi-tech.com>
2015-01-29 11:45:43 +0100

    Changed config key

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,7 +21,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('edsitech_gandi');
+        $rootNode = $treeBuilder->root('edsi_tech_gandi');
 
         $rootNode
             ->children()
